f34d4f638e82fe50908af4e161445656e41f7592
Yassir Dahraoui <dahraoui.yassir@educ.di.unito.it>
2026-07-27 23:54:16 +0200

    persistence features: added persistenceManager to read local files and tested in controller

--- src/main/java/fx/tradesjournal/controllers/OpeningController.java
+++ src/main/java/fx/tradesjournal/controllers/OpeningController.java
@@ -1,6 +1,8 @@
 package fx.tradesjournal.controllers;
 
+import fx.tradesjournal.persistence.PersistenceManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
@@ -13,14 +15,21 @@ public class OpeningController {
     private Label welcomeText;
 
     @FXML
+    private ComboBox<String> journals;
+
+    @FXML
     protected void onTradeButtonClick() {
         welcomeText.setText("Welcome to your trades journal!");
     }
 
     @FXML
     private void handleClose() {
-        // Chiude l'applicazione quando si clicca la X
         Stage stage = (Stage) titleBar.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    public void initialize() {
+        journals.getItems().addAll(PersistenceManager.getJournalFiles());
     }
 }
---
+++ src/main/java/fx/tradesjournal/persistence/PersistenceManager.java
@@ -0,0 +1,39 @@
+package fx.tradesjournal.persistence;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersistenceManager {
+    private static final String dataPath = "data";
+
+    public static File dataFolderExists(){
+        File folder = new File(dataPath);
+        if(!folder.exists()) folder.mkdirs();
+        return folder;
+    }
+
+    public static List<String> getJournalFiles(){
+        File folder = dataFolderExists();
+        List<String> journalNames = new ArrayList<String>();
+        File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".json"));
+        if (files != null) {
+            for (File file : files) {
+                String fileName = file.getName();
+                String cleanName = fileName.substring(0, fileName.length() - 5);
+                journalNames.add(cleanName);
+            }
+        }
+        return journalNames;
+    }
+
+    public static int getJournalsQt(){
+        File folder = dataFolderExists();
+        File[] files = folder.listFiles();
+        return files == null ? 0 : files.length;
+    }
+
+    public static boolean isDataFolderEmpty(){
+        return getJournalsQt() == 0;
+    }
+}
